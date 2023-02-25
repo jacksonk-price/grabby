cb19550d56ced17f301d3a0e1122171fdcbd6ac1
Jackson Price <price.jacksonk@gmail.com>
2023-02-25 14:03:04 -0600

    Add: funcitonality to remove duplicate images

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 let images = document.getElementsByTagName("img");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "getImages") {
-        console.log('hello world');
         let imageUrls = [];
         for (let i = 0; i < images.length; i++) {
             imageUrls.push(images[i].src);
         }
+
+        let uniqueUrls = [...new Set(imageUrls)];
     
-        sendResponse({ images: imageUrls });
+        sendResponse({ images: uniqueUrls });
     }
 });
