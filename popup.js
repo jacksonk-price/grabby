d7ea35a20d1cdffa04e38010e7217d8024639092
Jackson Price <price.jacksonk@gmail.com>
2023-02-25 13:36:18 -0600

    Refactor and add core functionality

--- popup.js
+++ popup.js
@@ -1,42 +1,39 @@
-
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   chrome.tabs.sendMessage(tabs[0].id, { action: "getImages" }, function (response) {
-    let images = response.images;
-    generateImagesDisplay(images);
+    const imageUrls = response.images;
+    appendImageCountToDOM(imageUrls.length);
+    appendImagesToDOM(imageUrls);
   });
 });
 
-const generateImagesDisplay = (images) => {
-  // refactor this mess
-  let srcCollection = images;
-  let selectedImagesList = document.body.querySelector('#image-container');
-  let headerDiv = document.getElementById('head');
-  pTag = document.createElement('p');
-  pTag.innerHTML = srcCollection.length + ' Images Found';
-  headerDiv.appendChild(pTag);
-  for (let i = 0; i < srcCollection.length; i++) {
-    imageDiv = createImageBox();
-    imageDiv.addEventListener('click', function(){
-      if (this.classList.contains('selected')){
-        this.classList.remove('selected');
-      } else {
-        this.classList.add('selected');
-      }
-    });
-    img = createImage(srcCollection[i]);
-    imageDiv.appendChild(img);
-    selectedImagesList.appendChild(imageDiv);
+const appendImagesToDOM = (imageUrls) => { 
+  const imageContainer = document.querySelector("#image-container");
+  for (let i = 0; i < imageUrls.length; i++) {
+    let imageBox = createImageBox();
+    let image = createImageElement(imageUrls[i]);
+    imageBox.appendChild(image);
+    imageContainer.appendChild(imageBox);
   }
 }
 
+const appendImageCountToDOM = (imageCount) => {
+  const headerDiv = document.getElementById('head');
+  pTag = document.createElement('p');
+  pTag.innerHTML = `${imageCount} Images Found`;
+  headerDiv.appendChild(pTag);
+}
+
 const createImageBox = () => { 
-  div = document.createElement('div');
+  let div = document.createElement('div');
+  div.addEventListener('click', function() {
+    selectBox(div);
+  });
   div.classList.add("image-box");
 
   return div;
 }
 
-const createImage = (src) => {
+const createImageElement = (src) => {
   let img = document.createElement('img');
   img.classList.add('collected-image');
   img.src = src;
@@ -44,5 +41,34 @@ const createImage = (src) => {
   return img;
 }
 
-console.log('made it to the end of script')
+const selectAll = () => {
+  let imageBoxes = document.querySelectorAll("div.image-box:not(.selected)");
 
+  for (let i = 0; i < imageBoxes.length; i++) {
+    imageBoxes[i].classList.toggle('selected');
+  }
+}
+
+const selectBox = (imageBox) => {
+  if (imageBox.classList.contains('selected')){
+    imageBox.classList.remove('selected');
+  } else {
+    imageBox.classList.add('selected');
+  }
+}
+
+const downloadSelected = () => {
+  selectedImages = document.querySelectorAll("div.image-box.selected img");
+  for (let i = 0; i < selectedImages.length; i++) {
+    let a = document.createElement('a');
+    a.href = selectedImages[i]['src'];
+    a.download = 'image';
+    a.setAttribute('target', '_self');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+}
+
+document.getElementById('select-all').addEventListener('click', selectAll);
+document.getElementById('download-btn').addEventListener('click', downloadSelected);
