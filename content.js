0e09b1384262e65d0f0481c3ce742a7419c36274
Jackson Price <price.jacksonk@gmail.com>
2023-03-04 09:40:35 -0600

    Check for empty src

--- content.js
+++ content.js
@@ -3,11 +3,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "getImages") {
         let imageUrls = [];
         for (let i = 0; i < images.length; i++) {
+          if (images[i].src !== '') {
             imageUrls.push(images[i].src);
+          }
         }
 
         let uniqueUrls = [...new Set(imageUrls)];
-    
+        
         sendResponse({ images: uniqueUrls });
     }
 });
